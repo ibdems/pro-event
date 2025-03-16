bd86f99152106bbee430223554dcb894e72fcfa3
ibdems <devibrahima88@gmail.com>
2025-03-16 13:18:10 +0000

    remove_email_password

--- config/settings.py
+++ config/settings.py
@@ -162,7 +162,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "ibrahima882001@gmail.com"
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_PASSWORD = ""
 DEFAULT_FROM_EMAIL = "ibrahima882001@gmail.com"
 EMAIL_USE_TLS = True
 
